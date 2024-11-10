22e2cd05bda7d08e10781e7888516e30ee785de2
İrfan Keleş <irfn.ak47@gmail.com>
2024-11-10 23:54:21 +0300

    blank compoenent header ataması yapıldı

--- src/app/@component/blank/blank.component.ts
+++ src/app/@component/blank/blank.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { CardModule } from 'primeng/card';
 @Component({
   selector: 'app-blank',
   standalone: true,
-  imports: [CardModule],
+  imports: [
+    CardModule,
+    CommonModule],
   templateUrl: './blank.component.html',
   styleUrl: './blank.component.scss'
 })
 export class BlankComponent {
-  @Input() cardTitle:string = "Default Card Title"
-
+  @Input() cardTitle:string;
 }
