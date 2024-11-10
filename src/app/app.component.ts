b1fcdba5998d53f8c23bd7861768c669cfb62a28
İrfan Keleş <irfn.ak47@gmail.com>
2024-11-10 23:15:38 +0300

    paket kurulumları yapıldı ve  header düzenlemeleri yapıldı

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { DropdownModule } from 'primeng/dropdown';
-import { FormsModule } from '@angular/forms';
+
 
 interface City {
   name: string;
@@ -13,24 +10,10 @@ interface City {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule,InputTextModule,DropdownModule, FormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'AngularDepoApp';
-
-  cities: City[] | undefined;
-
-  selectedCity: City | undefined;
-
-  ngOnInit() {
-      this.cities = [
-          { name: 'New York', code: 'NY' },
-          { name: 'Rome', code: 'RM' },
-          { name: 'London', code: 'LDN' },
-          { name: 'Istanbul', code: 'IST' },
-          { name: 'Paris', code: 'PRS' }
-      ];
-  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import("./@layout/main/main.component").then(x => x.MainComponent),
+        children: [
+            {
+                path: '',
+                loadComponent: () => import("./@component/main-container/main-container.component").then(x => x.MainContainerComponent),
+            }
+        ]
+    },
+    {
+        path: '**',
+        loadComponent: () => import("./@layout/not-found-404/not-found-404.component").then(x => x.NotFound404Component)
+    }
+];
