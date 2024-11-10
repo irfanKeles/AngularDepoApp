aeb67bd0a102bc5c9d2f914e1511d0ebaca3f3a9
İrfan Keleş <irfn.ak47@gmail.com>
2024-11-11 01:30:30 +0300

    reyon silme işlemi yapıldı

--- src/app/@component/generic-table/generic-table.component.ts
+++ src/app/@component/generic-table/generic-table.component.ts
@@ -5,6 +5,11 @@ import { TableModule } from 'primeng/table';
 import { Colum } from '../../@model/genericTable/table';
 import { WarehouseType } from '../../@model/enums/WarehouseTypeEnum';
 import { Warehouse } from '../../@model/warehouse';
+import { DialogService } from 'primeng/dynamicdialog';
+import { ConfirmationService } from 'primeng/api';
+import { NotificationService } from '../../@service/notification.service';
+import { SharedService } from '../../@service/shared.service';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 
 @Component({
   selector: 'app-generic-table',
@@ -12,13 +17,60 @@ import { Warehouse } from '../../@model/warehouse';
   imports: [
     TableModule,
     ButtonModule,
-    CommonModule
+    CommonModule,
+    ConfirmPopupModule
   ],
   templateUrl: './generic-table.component.html',
-  styleUrl: './generic-table.component.scss'
+  styleUrl: './generic-table.component.scss',
+  providers: [DialogService, ConfirmationService]
 })
 export class GenericTableComponent {
   @Input() cols: Colum[];
   @Input() tableData!: Warehouse[];
   @Input() warehouseType!: WarehouseType;
+  warehouseData: Warehouse[] = [];
+
+  constructor(
+    public dialogService: DialogService,
+    private sharedService: SharedService,
+    private confirmationService: ConfirmationService,
+    private notificationService: NotificationService
+  ) {
+    
+  }
+  ngOnInit() {
+    this.getInventory();
+  }
+
+  getInventory() {
+    const warehouseType = this.warehouseType;
+    const getWarehouse = warehouseType === 1 ? this.sharedService.warehouseA$ : this.sharedService.warehouseB$;
+    getWarehouse.subscribe(x => {
+      this.warehouseData = x;
+    });
+  }
+
+  deleteRayon(rowData: Warehouse, event: Event) {
+    const index = this.warehouseData.findIndex(item => item.id === rowData.id);
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: `${rowData.rayon} Silmek Üzeresiniz ?`,
+      icon: 'pi pi-info-circle',
+      acceptButtonStyleClass: 'p-button-danger p-button-sm',
+      accept: () => {
+        if (index !== -1) {
+          this.warehouseData.splice(index, 1);
+          if (this.warehouseType === 1) {
+            this.sharedService.setWarehouseA(this.warehouseData);
+          } else {
+            this.sharedService.setWarehouseA(this.warehouseData);
+          }
+        }
+        this.notificationService.success();
+      },
+      reject: () => {
+      }
+    });
+
+  }
 }
